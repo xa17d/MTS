c730d85c8c6d9227e81a981fd018ff7400563a97
Goran <goran@gogi.local>
2013-05-22 23:24:40 +0200

    added Getter and Setter in CdaDocument

--- MtsEntityLibrary/src/at/mts/entity/cda/CdaDocument.java
+++ MtsEntityLibrary/src/at/mts/entity/cda/CdaDocument.java
@@ -1,7 +1,8 @@
 package at.mts.entity.cda;
 
 import java.util.UUID;
-
+import java.util.Date;
+import at.mts.entity.Gender;
 /**
  * CDA-Dokument.
  * Ermoeglicht das einlesen oder erzeugen eines CDA-Dokuments.
@@ -87,16 +88,122 @@ public class CdaDocument {
 	 */
 	public void setPatientId(UUID value) { patientId = value; }
 	
-	// TODO: Private Variablen, Getter und Setter fuer:
-	// PatientId : UUID
-	// PatientNameGiven : String
-	// PatientNameFamily : String
-	// PatientGender : Geschlecht
-	// PatientBirthTime : Date
-	// AuthorId : UUID
-	// AuthorNameGiven : String
-	// AuthorNameFamily : String
 	
+	/**
+	 * Vorname des Patienten im Header
+	 */
+	private String patientNameGiven;
+	/**
+	 * @return Vorname des Patienten im Header
+	 */
+	public String getPatientNameGiven(){  return patientNameGiven;  }
+	/**
+	 * Setzt den Vornamen des Patienten im Header
+	 * @param name neuer Name
+	 */
+	private void setPatientNameGiven(String name){
+		patientNameGiven=name;
+	}
+	
+	/**
+	 * Nachname des Patienten im Header
+	 */
+	private String patientNameFamily;
+	/**
+	 * @return Nachname des Patienten im Header
+	 */
+	public String getPatientNameFamily(){  return patientNameFamily;  }
+	/**
+	 * Setzt den Nachnamen des Patienten im Header
+	 * @param name neuer Name
+	 */
+	private void setPatientNameFamily(String name){
+		patientNameFamily=name;
+	}
+
+	/**
+	 * Geburtsdatum des Patienten im Header
+	 */
+	private Date patientBirthTime;
+	/**
+	 * @return Geburtsdatum des Patienten im Header
+	 */
+	public Date getPatientBirthTime(){  return patientBirthTime;  }
+	/**
+	 * Setzt das Geburtsdatum des Patienten im Header
+	 * @param date neues Datum
+	 */
+	public void setPatientBirthTime(Date date){
+		patientBirthTime = date;
+	}	
+
+	/**
+	 * Geschlecht des Patienten im Header
+	 */
+	private Gender patientGender;
+	/**
+	 * @return Geschlecht des Patienten im Header
+	 */
+	public Gender getPatientGender(){  return patientGender;  }
+	/**
+	 * Setzt das Geschlecht des Patienten im Header
+	 * @param gender neues Geschlecht
+	 */
+	private void setPatientGender(Gender gender){
+		patientGender = gender;
+	}
+	
+	/**
+	 * Nachname des Authors im Header
+	 */
+	private String authorNameFamily;
+	/**
+	 * @return Nachname des Authors im Header
+	 */
+	public String getAuthorNameFamily(){  return authorNameFamily;  }
+	/**
+	 * Setzt den Nachnamen des Authors im Header
+	 * @param name neuer Name
+	 */
+	private void setAuthorNameFamily(String name){
+		authorNameFamily=name;
+	}
+
+	/**
+	 * Vorname des Authors im Header
+	 */
+	private String authorNameGiven;
+	/**
+	 * @return Vorname des Authors im Header
+	 */
+	public String getAuthorNameGiven(){  return authorNameGiven;  }
+	/**
+	 * Setzt den Vornamen des Authors im Header
+	 * @param name neuer Name
+	 */
+	private void setAuthorNameGiven(String name){
+		authorNameGiven=name;
+	}
+	
+	
+	
+	/**
+	 * ID des Authors im Header
+	 */
+	private UUID authorId;
+	/**
+	 * @return ID des Authors im Header
+	 */
+	public UUID getAuthorId(){  return authorId;  }
+	/**
+	 * Setzt die ID des Authors im Header
+	 * @param value neuer Wert
+	 */
+	public void setAuthorId(UUID value){
+		authorId = value;
+	}
+	
+
 	private CdaBody body = new CdaBody();
 	/**
 	 * @return CDA-Body
